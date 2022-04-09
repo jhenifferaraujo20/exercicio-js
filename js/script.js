7d08b4615b5a661baf397d79ecd7b9a1616e6d56
Jheniffer Araujo <jhenifferbaldi@gmail.com>
2022-04-08 21:29:01 -0300

    Novo desafio

--- js/script.js
+++ js/script.js
@@ -3,10 +3,15 @@ const btn = document.getElementById("btn");
 btn.onclick = () => {
     let nome = document.getElementById("nome");
     let lista = document.getElementById("lista").innerHTML;
+    let nomes = document.getElementById("nomes").innerHTML;
      
     if(nome.value != ""){
         lista += `<li>${nome.value}</li>`;
         document.getElementById("lista").innerHTML = lista;
+
+        nomes = nomes + nome.value;
+        document.getElementById("nomes").innerHTML = `${nomes}, `;
+
         nome.focus();
         nome.value = "";
     }else {
